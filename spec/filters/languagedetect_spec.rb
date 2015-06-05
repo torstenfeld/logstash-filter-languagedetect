adff102250259f0b52eab3480bfb4019d0593e2d
Torsten Feld <torsten@torsten-feld.de>
2015-06-05 17:35:55 +0200

    added tests for language_iso

--- spec/filters/languagedetect_spec.rb
+++ spec/filters/languagedetect_spec.rb
@@ -32,4 +32,35 @@ describe LogStash::Filters::Languagedetect do
       expect(subject['lang']).to eq(:spanish)
     end
   end
+
+  describe "with iso languages" do
+    let(:config) do <<-CONFIG
+      filter {
+        languagedetect {
+          useiso => true
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => "The links between the attempted car bombings in Glasgow and London are becoming clearer") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:en)
+    end
+
+    sample("message" => "dies ist ein deutscher Text") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:de)
+    end
+
+    sample("message" => "Deux autres personnes ont été arrêtées durant la nuit") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:fr)
+    end
+
+    sample("message" => "En estado de máxima alertaen su nivel de crítico") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:es)
+    end
+  end
 end
