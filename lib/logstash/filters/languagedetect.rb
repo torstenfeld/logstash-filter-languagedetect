7a50401105a1b8797ec4e56815e1a7a21dc6e091
Torsten Feld <torsten@torsten-feld.de>
2015-06-05 20:33:42 +0200

    strings instead of symbols are returned (forced UTF_8 encoding), added comments for config, adapted tests

--- lib/logstash/filters/languagedetect.rb
+++ lib/logstash/filters/languagedetect.rb
@@ -1,31 +1,58 @@
-# encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
 require 'whatlanguage'
-# require 'whatlanguage/string'
 
-# This example filter will replace the contents of the default 
-# message field with whatever you specify in the configuration.
-#
-# It is only intended to be used as an example.
+# This filter will try to determine the language of the field given
+# by the source parameter and output the result in either the language
+# name (in English lowercase) or iso format
 class LogStash::Filters::Languagedetect < LogStash::Filters::Base
 
   # Setting the config_name here is required. This is how you
   # configure this filter from your Logstash config.
   #
   # filter {
-  #   example {
-  #     message => "My message..."
+  #   languagedetect {
+  #     source => "message"
+  #     target => "lang"
+  #     useiso => true
   #   }
   # }
   #
   config_name "languagedetect"
   
-  # Replace the message with this value.
+  # Set the source field which is used for the language check.
+  #
+  # Example:
+  # [source,ruby]
+  #     filter {
+  #       languagedetect {
+  #         # tries to determine the language in "customfield"
+  #         source => "customfield"
+  #       }
+  #     }
   config :source, :validate => :string, :default => "message"
 
+  # Set the result field in which the result will be stored.
+  #
+  # Example:
+  # [source,ruby]
+  #     filter {
+  #       languagedetect {
+  #         # outputs the result into "customoutput"
+  #         target => "customoutput"
+  #       }
+  #     }
   config :target, :validate => :string, :default => "lang"
 
+  # If useiso is true, result will be "en" instead of "english"
+  #
+  # Example:
+  # [source,ruby]
+  #     filter {
+  #       languagedetect {
+  #         useiso => true
+  #       }
+  #     }
   config :useiso, :validate => :boolean, :default => false
 
 
@@ -40,13 +67,11 @@ class LogStash::Filters::Languagedetect < LogStash::Filters::Base
     text_to_detect = event[@source]
     if @useiso
       lang_found = @wl.language_iso(text_to_detect)
-      # lang_found = text_to_detect.language_iso
     else
       lang_found = @wl.language(text_to_detect)
-      # lang_found = text_to_detect.language
     end
 
-    event[@target] = lang_found
+    event[@target] = lang_found.to_s.force_encoding(Encoding::UTF_8)
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
--- spec/filters/languagedetect_spec.rb
+++ spec/filters/languagedetect_spec.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 require File.absolute_path(File.join(File.dirname(__FILE__), '../../spec/spec_helper'))
 require File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/logstash/filters/languagedetect'))
-# require "lib/logstash/filters/languagedetect"
 
 describe LogStash::Filters::Languagedetect do
   describe "with defaults and non iso languages" do
@@ -14,22 +13,22 @@ describe LogStash::Filters::Languagedetect do
 
     sample("message" => "The links between the attempted car bombings in Glasgow and London are becoming clearer") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:english)
+      expect(subject['lang']).to eq("english")
     end
 
     sample("message" => "dies ist ein deutscher Text") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:german)
+      expect(subject['lang']).to eq("german")
     end
 
     sample("message" => "Deux autres personnes ont été arrêtées durant la nuit") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:french)
+      expect(subject['lang']).to eq("french")
     end
 
     sample("message" => "En estado de máxima alertaen su nivel de crítico") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:spanish)
+      expect(subject['lang']).to eq("spanish")
     end
   end
 
@@ -45,22 +44,56 @@ describe LogStash::Filters::Languagedetect do
 
     sample("message" => "The links between the attempted car bombings in Glasgow and London are becoming clearer") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:en)
+      expect(subject['lang']).to eq("en")
     end
 
     sample("message" => "dies ist ein deutscher Text") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:de)
+      expect(subject['lang']).to eq("de")
     end
 
     sample("message" => "Deux autres personnes ont été arrêtées durant la nuit") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:fr)
+      expect(subject['lang']).to eq("fr")
     end
 
     sample("message" => "En estado de máxima alertaen su nivel de crítico") do
       expect(subject).to include('lang')
-      expect(subject['lang']).to eq(:es)
+      expect(subject['lang']).to eq("es")
+    end
+  end
+
+  describe "non default target" do
+    let(:config) do <<-CONFIG
+      filter {
+        languagedetect {
+          useiso => true
+          target => 'test'
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => "The links between the attempted car bombings in Glasgow and London are becoming clearer") do
+      expect(subject).to include('test')
+      expect(subject['test']).to eq("en")
+    end
+  end
+
+  describe "non default source" do
+    let(:config) do <<-CONFIG
+      filter {
+        languagedetect {
+          useiso => true
+          source => 'test'
+        }
+      }
+    CONFIG
+    end
+
+    sample("test" => "The links between the attempted car bombings in Glasgow and London are becoming clearer") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq("en")
     end
   end
 end
