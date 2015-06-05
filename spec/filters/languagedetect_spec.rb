e12ffdf2c6bb1cfbac5af775d2165a2b5ab161d8
Torsten Feld <torsten@torsten-feld.de>
2015-06-05 15:44:31 +0200

    added basic tests

--- spec/filters/languagedetect_spec.rb
+++ spec/filters/languagedetect_spec.rb
@@ -1,20 +1,35 @@
-require 'spec_helper'
-require "lib/logstash/filters/languagedetect"
+# encoding: utf-8
+require File.absolute_path(File.join(File.dirname(__FILE__), '../../spec/spec_helper'))
+require File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/logstash/filters/languagedetect'))
+# require "lib/logstash/filters/languagedetect"
 
 describe LogStash::Filters::Languagedetect do
-  describe "Set to Hello World" do
+  describe "with defaults and non iso languages" do
     let(:config) do <<-CONFIG
       filter {
-        languagedetect {
-          message => "Hello World"
-        }
+        languagedetect {}
       }
     CONFIG
     end
 
-    sample("message" => "some text") do
-      expect(subject).to include("message")
-      expect(subject['message']).to eq('Hello World')
+    sample("message" => "The links between the attempted car bombings in Glasgow and London are becoming clearer") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:english)
+    end
+
+    sample("message" => "dies ist ein deutscher Text") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:german)
+    end
+
+    sample("message" => "Deux autres personnes ont été arrêtées durant la nuit") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:french)
+    end
+
+    sample("message" => "En estado de máxima alertaen su nivel de crítico") do
+      expect(subject).to include('lang')
+      expect(subject['lang']).to eq(:spanish)
     end
   end
 end
